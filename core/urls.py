b2f53f9930718c0c74d2276d673b303b800f97f4
ShagiakhmedovA <almaz.2014@mail.ru>
2020-12-25 22:51:07 +0300

    login

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sign_in', views.sign_in, name="sign_in"),
+    path('sign_up', views.sign_up, name="sign_up"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'main/landing.html', {})
+
+
+def sign_in(request):
+    return render(request, 'main/sign_in.html', {})
+
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        # Правильный пароль и пользователь "активен"
+        auth.login(request, user)
+        # Перенаправление на "правильную" страницу
+        return render(request, 'main/landing.html', {})
+    else:
+        # Отображение страницы с ошибкой
+        return HttpResponseRedirect("/account/invalid/")
+
+
+def logout(request):
+    auth.logout(request)
+    # Перенаправление на страницу.
+    return HttpResponseRedirect("/account/loggedout/")
+
+
+def sign_up(request):
+    return render(request, 'main/sign_up.html', {})
