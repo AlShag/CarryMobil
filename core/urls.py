0695a165f8db033e0035c986c840d35da4a3c878
Arthur2101 <artur.ganiev.2001@mail.ru>
2020-12-30 15:39:28 +0300

    login

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('sign_up', views.sign_up, name="register"),
+    path('order', views.order, name='order'),
+]
---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+
+
+def index(request):
+    return render(request, 'main/landing.html', {})
+
+
+def sign_in(request):
+    return render(request, 'accounts/login.html', {})
+
+
+def sign_up(request):
+    return render(request, 'accounts/register.html')
+
+def order(request):
+    return render(request, 'main/order.html')
